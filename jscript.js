7dbdfdf35b9f9939301d960d74a0dc31e0eac143
MarieMakesMoneys <marie.collecchia@gmail.com>
2022-11-10 16:34:42 +0100

    Display content of gameboard array in cells

--- jscript.js
+++ jscript.js
@@ -1,1 +1,26 @@
+const Player = (playerName, playerLogo) => {
+    //let //blabla
+    //return //blabla
+}
 
+const gameBoard = (function() {
+    let gameboard = ['x', 'x', 'x', 'o', 'x', , 'o', 'o', 'o']
+
+    function displayGameboard() {
+        let cells = document.querySelectorAll('div.cell')
+        cells.forEach((cell) => {
+            let arrayIndex = cell.getAttribute('data-array-index')
+            console.log(arrayIndex)
+            cell.textContent = gameboard[arrayIndex]
+        })
+    }
+
+    return {gameboard, displayGameboard}
+})()
+
+gameBoard.displayGameboard()
+
+const displayController = (function() {
+    //let //blabla
+    //return //blabla
+})()
